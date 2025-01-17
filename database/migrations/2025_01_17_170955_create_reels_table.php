1c4125a7d15cf6a69c7baace102dd40790269fbe
david3010M <hvaldiviezos@unprg.edu.pe>
2025-01-17 13:23:54 -0500

    Update 2025_01_17_170955_create_reels_table.php

--- database/migrations/2025_01_17_170955_create_reels_table.php
+++ database/migrations/2025_01_17_170955_create_reels_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('reels', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
             $table->string('video');
             $table->boolean('active')->default(true);
             $table->foreignId('product_id')->nullable()->constrained();

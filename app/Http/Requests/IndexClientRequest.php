a3253e88c1f98e36442ccf516303e2dda53a3959
david3010M <hvaldiviezos@unprg.edu.pe>
2025-07-02 00:01:07 -0500

    feat: validation index clients

--- app/Http/Requests/IndexClientRequest.php
+++ app/Http/Requests/IndexClientRequest.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Product;
+
 class IndexClientRequest extends IndexRequest
 {
     public function rules(): array
     {
+        $namesProducts = Product::where('active', true)
+            ->pluck('name')
+            ->toArray();
         return [
-            'product' => "required|string|in:Gesrest,360sys,HotelHUB,Comprobante-e,Mr. Soft",
+            // 'product' => "required|string|in:Gesrest,360sys,HotelHUB,Comprobante-e,Mr. Soft",
+            'product' => [
+                'required',
+                'string',
+                'in:' . implode(',', $namesProducts),
+            ],
             'limit' => "nullable|integer|min:1",
         ];
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,12 +13,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            //            CategorySeeder::class,
-//            ProductSeeder::class,
+            // CategorySeeder::class,
+            // ProductSeeder::class,
             // DepartmentSeeder::class,
-//            TypeSeeder::class,
-//            ClientSeeder::class,
-            ClientGesrestSeeder::class
+            // TypeSeeder::class,
+            // ClientSeeder::class,
+            // ClientGesrestSeeder::class
 
         ]);
     }
